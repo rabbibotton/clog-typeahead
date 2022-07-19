7bfa6b51ac5c7e593eb3cc82a15e1ae0387af932
David Botton <david@botton.com>
2022-07-18 22:57:36 -0400

    untabbify

--- clog-typeahead-tools.lisp
+++ clog-typeahead-tools.lisp
@@ -3,40 +3,40 @@
 (progn
   (clog-tools:add-supported-controls
    (list `(;; unique name to control used to identify it the .clog xml
-	   :name           "input-typeahead"
-	   ;; how control appears in builder control list 
-	   :description    "Typeahead.js Input"
-	   ;; the common lisp type of the control
-	   :clog-type      clog-typeahead:clog-typeahead-element
-	   ;; the create-function used to create the function
-	   ;; at _design time_ at run time only clog:attach-as-child is used
-	   ;; any initialization at _run time_ is done with :on-setup below.
-	   ;; in this case we just use a plain form input for design time.
-	   :create         clog:create-form-element
-	   ;; clog has the following create-types
-	   ;;   :base         - create
-	   ;;   :element      - create create-content
-	   ;;   :form         - create create-param create-value
-	   ;;   :text-area    - create create-value
-	   ;;   :custom-query - create (ask user for string)
-	   ;;   :custom       - create create-content
-	   :create-type    :form
-	   :create-param   :text
-	   :create-value   ""
-	   ;; setup the control at _design time_ and custom attributes
-	   :setup          ,(lambda (control content control-record)
+           :name           "input-typeahead"
+           ;; how control appears in builder control list
+           :description    "Typeahead.js Input"
+           ;; the common lisp type of the control
+           :clog-type      clog-typeahead:clog-typeahead-element
+           ;; the create-function used to create the function
+           ;; at _design time_ at run time only clog:attach-as-child is used
+           ;; any initialization at _run time_ is done with :on-setup below.
+           ;; in this case we just use a plain form input for design time.
+           :create         clog:create-form-element
+           ;; clog has the following create-types
+           ;;   :base         - create
+           ;;   :element      - create create-content
+           ;;   :form         - create create-param create-value
+           ;;   :text-area    - create create-value
+           ;;   :custom-query - create (ask user for string)
+           ;;   :custom       - create create-content
+           :create-type    :form
+           :create-param   :text
+           :create-value   ""
+           ;; setup the control at _design time_ and custom attributes
+           :setup          ,(lambda (control content control-record)
                               (declare (ignore content) (ignore control-record))
                               (setf (attribute control "data-on-typeahead")
-				    "(clog-typeahead:test-match data `(\"one\" \"two\" \"three\"))"))
-	   ;; code to run at _run time_ after all controls attached to panel
-	   :on-setup       ,(lambda (control control-record)
+                                    "(clog-typeahead:test-match data `(\"one\" \"two\" \"three\"))"))
+           ;; code to run at _run time_ after all controls attached to panel
+           :on-setup       ,(lambda (control control-record)
                               (declare (ignore control control-record))
-			      (format nil "(clog-typeahead:attach-typeahead target nil)"))
-	   ;; events handled
+                              (format nil "(clog-typeahead:attach-typeahead target nil)"))
+           ;; events handled
            :events         ((:name        "on-typeahead"
                              :package     "clog-typeahead"
                              :parameters  "target data")
-			    ,@clog-tools::*events-element*)
-	   ;; properties handled
-	   :properties     (,@clog-tools::*props-form-element*))))
+                            ,@clog-tools::*events-element*)
+           ;; properties handled
+           :properties     (,@clog-tools::*props-form-element*))))
   (format t "~%CLOG-TYPEAHEAD installed in CLOG Builder"))
--- clog-typeahead.lisp
+++ clog-typeahead.lisp
@@ -1,12 +1,12 @@
 (defpackage #:clog-typeahead
   (:use #:cl #:clog)
   (:export init-typeahead
-	   attach-typeahead
-	   start-test
-	   test-match
-	   clog-typeahead-element
-	   create-typeahead-element
-	   set-on-typeahead))
+           attach-typeahead
+           start-test
+           test-match
+           clog-typeahead-element
+           create-typeahead-element
+           set-on-typeahead))
 
 (in-package :clog-typeahead)
 
@@ -20,7 +20,7 @@
 
 (defgeneric create-typeahead-element (clog-obj element-type
                                       &key name value label class
-					hidden html-id)
+                                        hidden html-id)
   (:documentation "Create a new clog-typeahead-element as child of CLOG-OBJ."))
 
 (defmethod create-typeahead-element ((obj clog:clog-obj) element-type
@@ -31,17 +31,17 @@
                                        (hidden nil)
                                        (html-id nil))
   (let ((new-obj (create-form-element obj element-type
-				      :name name
-				      :value value
-				      :label label
-				      :class class
-				      :hidden hidden
-				      :html-id html-id)))
+                                      :name name
+                                      :value value
+                                      :label label
+                                      :class class
+                                      :hidden hidden
+                                      :html-id html-id)))
     (attach-typeahead new-obj nil)
     (change-class new-obj 'clog-typeahead-element)))
 
 (defgeneric set-on-typeahead (clog-obj handler
-			      &key cancel-event one-time)
+                              &key cancel-event one-time)
   (:documentation "Set a HANDLER for typeahead data queries on CLOG-OBJ.
  If HANDLER is nil unbind all event handlers. Handler is called with a
 data parameter that is the current query, ie the text in the control.
@@ -49,20 +49,20 @@ The return value of this handler must be a list that will be the possible
 values based on that query."))
 
 (defmethod set-on-typeahead ((obj clog:clog-obj) handler
-			     &key
-			       (cancel-event nil)
-			       (one-time     nil))
+                             &key
+                               (cancel-event nil)
+                               (one-time     nil))
   (set-on-event-with-data obj "clog-typeahead-data"
-			  (lambda (obj query)
-					; clog['~A-ab'] contains the async function
-					; set on typing to call with typeahead data
-			    (js-execute obj (format nil "clog['~A-ab']([~{~A~}])"
-						    (html-id obj)
-						    (mapcar (lambda (s)
-							      (format nil "'~A'," s))
-							    (funcall handler obj query)))))
-			  :cancel-event cancel-event
-			  :one-time      one-time))
+                          (lambda (obj query)
+                                        ; clog['~A-ab'] contains the async function
+                                        ; set on typing to call with typeahead data
+                            (js-execute obj (format nil "clog['~A-ab']([~{~A~}])"
+                                                    (html-id obj)
+                                                    (mapcar (lambda (s)
+                                                              (format nil "'~A'," s))
+                                                            (funcall handler obj query)))))
+                          :cancel-event cancel-event
+                          :one-time      one-time))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Implementation - typeahead bindings to typeahead.js
@@ -74,12 +74,12 @@ attach-typeahead automatically, so generally no need to call."
   (check-type obj clog:clog-obj)
   ;; Only init once
   (load-script (html-document (connection-data-item obj "clog-body"))
-	       "/js/typeahead.jquery.js" :load-only-once t))
+               "/js/typeahead.jquery.js" :load-only-once t))
 
 (defun attach-typeahead (obj data
-			 &key (hint t) (highlight t)
-			   (minimum-length 1)
-			   (maximum-suggestions 10))
+                         &key (hint t) (highlight t)
+                           (minimum-length 1)
+                           (maximum-suggestions 10))
   "Attach typeahead to a form input. DATA is a list of possible values
 or a handler function with data, data being the form input and the
 handlers return value is list or nil to set handler with set-on-typeahead.
@@ -93,16 +93,16 @@ within the suggestion are highlighted."
   ;; Setup typeahead
   (jquery-execute obj
     (format nil
-	    "typeahead({hint: ~A, highlight: ~A, minLength: ~A},~
+            "typeahead({hint: ~A, highlight: ~A, minLength: ~A},~
                        {limit: ~A, ~
                          ~A
                          }})"
-	    (p-true-js hint)
-	    (p-true-js highlight)
-	    minimum-length
-	    maximum-suggestions
-	    (typecase data
-	      (cons (format nil "source: function findMatches(q, cb){~
+            (p-true-js hint)
+            (p-true-js highlight)
+            minimum-length
+            maximum-suggestions
+            (typecase data
+              (cons (format nil "source: function findMatches(q, cb){~
                          var matches=[];~
                          var sr=new RegExp(q,'i');~
                          var strs=[~{~A~}];~
@@ -111,16 +111,16 @@ within the suggestion are highlighted."
                              matches.push(str);~
                            }});~
                          cb(matches);"
-			    (mapcar (lambda (s)
-				      (format nil "'~A'," s))
-				    data)))
-	      ;; Use callback if data is nil or data is function
-	      (t    (format nil "async: true, source: function findMatches(q, cb, ab){~
+                            (mapcar (lambda (s)
+                                      (format nil "'~A'," s))
+                                    data)))
+              ;; Use callback if data is nil or data is function
+              (t    (format nil "async: true, source: function findMatches(q, cb, ab){~
                          ~A.trigger('clog-typeahead-data',q);
                          cb([]);
                          clog['~A-ab']=ab;"
-			    (jquery obj)
-			    (html-id obj)))))))
+                            (jquery obj)
+                            (html-id obj)))))))
 
 (defun test-match (data choices)
   "Simple filter for typeahead"
@@ -173,18 +173,18 @@ within the suggestion are highlighted."
   background-color: #fff;
 ")
   (let ((element  (create-form-element body :text))
-	(element2 (create-form-element body :text)))
+        (element2 (create-form-element body :text)))
     ;; Using element list
     (add-class element "typeahead")
     (attach-typeahead element '("one" "two" "three" "four"))
     ;; Using lisp function to return typeahead
     (add-class element2 "typeahead")
     (attach-typeahead element2 (lambda (obj data)
-				 (declare (ignore obj))
-				 (test-match data '("one" "two" "three" "four"))))))
+                                 (declare (ignore obj))
+                                 (test-match data '("one" "two" "three" "four"))))))
 
 (defun start-test ()
   (initialize 'test-typeahead
    :static-root (merge-pathnames "./www/"
-		  (asdf:system-source-directory :clog-typeahead)))
+                  (asdf:system-source-directory :clog-typeahead)))
   (open-browser))
